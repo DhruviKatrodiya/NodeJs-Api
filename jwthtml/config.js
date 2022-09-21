83182c503a94562ee3842db577859b276a915784
Dhruvi Katrodiya <103128211+DhruviKatrodiya@users.noreply.github.com>
2022-09-21 19:56:59 +0530

    Add files via upload

---
+++ jwthtml/config.js
@@ -0,0 +1,5 @@
+//mongoose config
+module.exports = {
+    database: 'mongodb+srv://dhruvikatrodiya:dhruvi@cluster0.t6jedsj.mongodb.net/studentdb?retryWrites=true&w=majority',
+    secret: 'yoursecret'
+}
